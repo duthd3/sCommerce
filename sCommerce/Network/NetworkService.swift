cf1dbfb62b3e292a3aa9eecb842fe8554d6d7155
duthd3 <duthd3@naver.com>
2025-06-04 22:01:05 +0900

    network 서비스 함수화]

--- sCommerce/Network/NetworkService.swift
+++ sCommerce/Network/NetworkService.swift
@@ -10,16 +10,26 @@ import Foundation
 class NetworkService {
     static let shared = NetworkService()
     
-    func getHomeData() async throws -> HomeResponse {
-        let urlString = "https://my-json-server.typicode.com/JeaSungLee/JsonAPIFastCampus/db"
+    private let hostURL = "https://my-json-server.typicode.com/JeaSungLee/JsonAPIFastCampus"
+    private func createURL(withPath path: String) throws -> URL {
+        let urlString: String = "\(hostURL)/\(path)"
         guard let url = URL(string: urlString) else {
             throw URLError(.badURL)
         }
-        
+        return url
+    }
+    
+    private func fetchData(from url: URL) async throws -> Data {
         let (data, response) = try await URLSession.shared.data(from: url)
         guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
             throw URLError(.badServerResponse)
         }
+        return data
+    }
+    
+    func getHomeData() async throws -> HomeResponse {
+        let url = try createURL(withPath: "/db")
+        let data = try await fetchData(from: url)
         let decodeData = try JSONDecoder().decode(HomeResponse.self, from: data)
         return decodeData
     }
